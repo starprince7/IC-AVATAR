dc3921cc2157ee1879e509d7792174073e5a8c51
prince_nweke <princeagezinweke@gmail.com>
2022-05-23 01:18:04 +0100

    First DApp commit

---
+++ src/avatar_assets/src/App.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { avatar } from "../../declarations/avatar";
+
+function App() {
+    return (
+        <div>
+            <h2 className="text-red-500">Internet Computer Avatar</h2>
+            <br />
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default App
